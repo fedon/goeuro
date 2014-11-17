8fe556071247a899ef45034329c76ace7e86e910
Dmytro <fedongm@gmail.com>
2014-11-17 18:42:47 +0100

    App: json provider

--- src/main/java/org/fedon/goeuro/client/GoeuroTestClient.java
+++ src/main/java/org/fedon/goeuro/client/GoeuroTestClient.java
@@ -7,6 +7,7 @@ import org.fedon.goeuro.model.GeoSuggest;
 import org.fedon.goeuro.rest.ExternalGeoService;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.proxy.WebResourceFactory;
+import org.glassfish.jersey.jackson.JacksonFeature;
 
 /**
  * @author Dmytro Fedonin
@@ -19,7 +20,7 @@ public class GoeuroTestClient {
         if (host == null)
             host = defaulHost;
         String base = "http://" + host + "/api/v2";
-        ClientConfig cc = new ClientConfig();// .register(JacksonFeature.class);
+        ClientConfig cc = new ClientConfig().register(JacksonFeature.class);
         Client resource = ClientBuilder.newClient(cc);
         ExternalGeoService client = WebResourceFactory.newResource(ExternalGeoService.class, resource.target(base));
 
