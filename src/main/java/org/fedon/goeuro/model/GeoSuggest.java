868402eadae344969be773798103e7c97a0b9d7e
Dmytro <fedongm@gmail.com>
2014-11-17 17:18:12 +0100

    Model: unconventional mapping

--- src/main/java/org/fedon/goeuro/model/GeoSuggest.java
+++ src/main/java/org/fedon/goeuro/model/GeoSuggest.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -14,14 +15,18 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class GeoSuggest {
 
+    @XmlElement(name = "_id")
     long id;
     long key;
     String name;
     String fullName;
+    @XmlElement(name = "iata_airport_code")
     int airportCodeIATA;
     String type;
     String country;
+    @XmlElement(name = "geo_position")
     GeoPosition position;
+    @XmlElement(name = "location_id")
     long locationId;
     boolean inEurope;
     String countryCode;
