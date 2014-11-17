493d5c4974b7c3afd761493fada372e9078e64ca
Dmytro <fedongm@gmail.com>
2014-11-17 17:47:31 +0100

    App: main trivial

--- src/main/java/org/fedon/goeuro/App.java
+++ src/main/java/org/fedon/goeuro/App.java
@@ -1,5 +1,8 @@
 package org.fedon.goeuro;
 
+import org.fedon.goeuro.client.GoeuroTestClient;
+import org.fedon.goeuro.dao.GeoCsvDao;
+
 /**
  * Main application class.
  * 
@@ -8,7 +11,16 @@ package org.fedon.goeuro;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+ {
+        String pattern = null;
+        String host = null;
+        if (args.length > 0)
+            pattern = args[0];
+        else
+            System.out.println("Usage: pattern [host]\nWhere pattern is a location name to search.");
+        if (args.length > 1)
+            host = args[1];
+        // TODO consider Guice
+        new GeoCsvDao().write(new GoeuroTestClient().callExternalGeoSuggestService(pattern, host));
     }
 }
