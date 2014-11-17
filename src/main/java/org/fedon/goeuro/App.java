e43c9b12605ef3d34b995b935dc4a3cc232ee1a1
Dmytro <fedongm@gmail.com>
2014-11-17 14:46:27 +0100

    Maven project init

---
+++ src/main/java/org/fedon/goeuro/App.java
@@ -0,0 +1,13 @@
+package org.fedon.goeuro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
